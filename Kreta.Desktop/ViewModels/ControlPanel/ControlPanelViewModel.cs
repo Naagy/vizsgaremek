850a742edabb085d57f4f9834dc014e6aeff37cf
gycsaba <gycsaba@SZT9-24>
2025-01-23 09:07:33 +0100

    Dependency injection

--- Kreta.Desktop/ViewModels/ControlPanel/ControlPanelViewModel.cs
+++ Kreta.Desktop/ViewModels/ControlPanel/ControlPanelViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService;
+using System;
 
 namespace Kreta.Desktop.ViewModels.ControlPanel
 {
     public class ControlPanelViewModel: BaseViewModel
     {
+        private readonly IStudentHttpService _studentHttpService;
+        public ControlPanelViewModel(IStudentHttpService? studentHttpService)
+        {
+            _studentHttpService=studentHttpService ?? throw new ArgumentException(nameof(studentHttpService));
+        }
+
 
     }
 }
