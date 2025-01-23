34d6a899be11992814cffbbbdde465620e3f9ee9
gycsaba <gycsaba@SZT9-24>
2025-01-23 09:06:34 +0100

    A view is elkészül

--- Kreta.Desktop/ViewModels/ControlPanel/ControlPanelViewModel.cs
+++ Kreta.Desktop/ViewModels/ControlPanel/ControlPanelViewModel.cs
@@ -1,12 +1,10 @@
 ﻿using Kreta.Desktop.ViewModels.Base;
+using Kreta.HttpService;
 
 namespace Kreta.Desktop.ViewModels.ControlPanel
 {
     public class ControlPanelViewModel: BaseViewModel
     {
-        public ControlPanelViewModel()
-        {
-                
-        }
+
     }
 }
